e75d378536cb63ab1e7efa0249a381b4c38533b1
Jatin Mayekar <44420409+jatinmayekar@users.noreply.github.com>
2024-06-24 23:54:00 -0400

    Add openai api key input, trail mode, reset app button

--- v1.0.py
+++ v1.0.py
@@ -1,33 +1,102 @@
 from openai import OpenAI
 import streamlit as st
+import tiktoken
+import json
 
 st.title("CareerBuddy")
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+enc = tiktoken.encoding_for_model("gpt-4o")
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4o"
-
-if "messages" not in st.session_state:
+def reset_app():
+    st.session_state["openai_api_key"] = ""
     st.session_state.messages = []
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+    """Return the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+        }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model:
+        print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+    elif "gpt-4" in model:
+        print("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
+        return num_tokens_from_messages(messages, model="gpt-4-0613")
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
         )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
+
+if "b_openai_api_key" not in st.session_state:
+    st.session_state["b_openai_api_key"] = False
+
+with st.sidebar:
+    openai_api_key = st.text_input('OpenAI API Key', type='password', key='openai_api_key')
+
+    if st.button('Enter'):
+        if not openai_api_key.startswith('sk-'):
+            st.warning('Please enter a valid OpenAI API key!', icon='⚠')
+        else:
+            if "client" not in st.session_state:
+                st.session_state["client"] = OpenAI(api_key=openai_api_key)
+
+    if st.button('Trial'):
+        if "client" not in st.session_state:
+            st.session_state["client"] = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+        
+        st.session_state["b_openai_api_key"] = True
+        # add a token counter later to limit the number of tokens used per session
+
+    st.button('Reset', on_click=reset_app)
+
+if st.session_state["b_openai_api_key"] == True:
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-4o"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            stream = st.session_state["client"].chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
